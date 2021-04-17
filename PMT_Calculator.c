ff1e6691715d9f21967b6dd6068e50d8692d7de3
Murilo Moraes <66094955+Moraes97-Murilo@users.noreply.github.com>
2021-04-17 02:41:43 -0300

    Update PMT_Calculator.c
    Uploading portuguese script

--- PMT_Calculator.c
+++ PMT_Calculator.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
+int main(){
+    float pv, i, n, pmt;
+    printf("Digite o saldo devedor: ");
+    scanf("%f", &pv);
+    printf("Digite o numero de parcelas: ");
+    scanf("%f", &n);
+    printf("Digite a taxa de juros: ");
+    scanf("%f", &i);
+    i = i /100 ;
+    pmt = pv*(((pow((1+i),n))*i)/((pow((1+i),n))-1));
+    
+    printf("O valor das parcelas ficou %f reais. \n", pmt);
+system("pause");
+}
